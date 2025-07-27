612265e9256b433f9e891f14d9cb1e11412bcea5
Felipe DR <felipe.corp7@gmail.com>
2025-07-27 13:44:02 -0300

    feat(features): implements AddressAndGeolocationModel

---
+++ src/features/locations/models/address-and-geolocation.model.ts
@@ -0,0 +1,9 @@
+export interface AddressAndGeolocationModel {
+  postalCode: string
+  street: string
+  city: string
+  neighborhood: string
+  state: string
+  latitude: number
+  longitude: number
+}
